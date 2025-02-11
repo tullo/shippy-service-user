b1bb1313b8bda6ff363b917d91531f9339d11742
Andreas <704542+tullo@users.noreply.github.com>
2025-02-11 22:22:10 +0100

    Update database.go

--- database.go
+++ database.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
-	"github.com/micro/micro/v3/service/config"
-	"github.com/micro/micro/v3/service/logger"
+	"github.com/micro/micro/v5/service/config"
+	"github.com/micro/micro/v5/service/logger"
 )
 
 // NewConnection returns a new database connection instance
